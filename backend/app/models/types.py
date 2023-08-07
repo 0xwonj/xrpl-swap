86d1680e065fcb8b6d186d1159b268b8ccc38dfd
wonj1012 <jjaa1012@gmail.com>
2023-08-08 00:06:47 +0900

    ✨feat: Add function to check Address validity

--- backend/app/models/types.py
+++ backend/app/models/types.py
@@ -1,10 +1,19 @@
 from typing import Dict, Any
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
+
+from xrpl.core.addresscodec import is_valid_classic_address, is_valid_xaddress
 
 
 class Address(BaseModel):
-    value: str = Field(..., min_length=34, max_length=34)
+    value: str = Field(..., min_length=25, max_length=58)
+
+    @validator("value")
+    # pylint: disable=no-self-argument
+    def validate_address(cls, value: str):
+        if not is_valid_classic_address(value) and not is_valid_xaddress(value):
+            raise ValueError("Invalid address")
+        return value
 
 
 class Result(BaseModel):

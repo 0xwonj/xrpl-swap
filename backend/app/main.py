c61fa905b2719d93748fbbd501ada6a2e6fdd7fb
wonj1012 <jjaa1012@gmail.com>
2023-08-07 17:33:20 +0900

    ✨feat: Add account router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from api.endpoints.account import router as account_router
 
 app = FastAPI()
+
+app.include_router(
+    account_router,
+    prefix="/api/v1/account",
+    tags=["account"],
+    responses={404: {"description": "Not found"}},
+)

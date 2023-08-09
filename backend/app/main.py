04b48ce633c5f4de72d8e7233510061fe4fea66c
wonj1012 <jjaa1012@gmail.com>
2023-08-09 10:20:21 +0900

    ✨feat: Change router prefix rules

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from api.endpoints.account import router as account_router
 app = FastAPI()
 
 
-common_router_config = {"prefix": "/api/v1"}
+api_router_config = {"prefix": "/api"}
+ws_router_config = {"prefix": "/ws"}
 
-app.include_router(account_router, **common_router_config)  # type: ignore
+app.include_router(account_router, **api_router_config)  # type: ignore

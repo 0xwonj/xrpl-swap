a5ee9268a17bbc0da0aceec08136f05af27b5434
wonj1012 <jjaa1012@gmail.com>
2023-08-09 10:16:42 +0900

    ✨feat: Using .env for xrpl server url

--- backend/app/xrpledger/client.py
+++ backend/app/xrpledger/client.py
@@ -1,13 +1,30 @@
+import os
+from dotenv import load_dotenv
+
 from xrpl.asyncio.clients import AsyncJsonRpcClient
 
-from constants import TESTNET_URL
+from constants import TESTNET_URL, MAINNET_URL, AMMDEVNET_URL
+
+load_dotenv()
 
 
-async def get_xrpl_client() -> AsyncJsonRpcClient:
+def get_url() -> str:
+    env = os.getenv("XRPL_ENVIRONMENT", "MAINNET")
+
+    return {
+        "TESTNET": TESTNET_URL,
+        "MAINNET": MAINNET_URL,
+        "DEVNET": AMMDEVNET_URL,
+    }.get(env, MAINNET_URL)
+
+
+async def get_xrpl_client(url: str | None = None) -> AsyncJsonRpcClient:
     """
     Returns an AsyncJsonRpcClient instance connected to the XRPL (XRP Ledger) testnet.
 
     Returns:
         AsyncJsonRpcClient: An instance of AsyncJsonRpcClient connected to the XRPL testnet.
     """
-    return AsyncJsonRpcClient(TESTNET_URL)
+    if url is None:
+        url = get_url()
+    return AsyncJsonRpcClient(url)

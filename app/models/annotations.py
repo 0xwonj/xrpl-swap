531f5f55a15b8661d0189ae7aba9c9f294e3e16b
wonj1012 <jjaa1012@gmail.com>
2023-08-11 00:01:02 +0900

    ✨feat: Add annotations

---
+++ app/models/annotations.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Path, Depends
+
+from xrpl.asyncio.clients import AsyncJsonRpcClient
+
+from app.common.constants import XRPL_ADDRESS_REGEX
+from app.common.config import settings
+from app.xrpl.client import get_xrpl_client
+
+
+XrplAddress = Annotated[
+    str,
+    Path(
+        ...,
+        description="The XRPL address of the user.",
+        example=settings.wallet.address,
+        pattern=XRPL_ADDRESS_REGEX,
+    ),
+]
+
+XrplClient = Annotated[AsyncJsonRpcClient, Depends(get_xrpl_client)]

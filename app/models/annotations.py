260e3a4ae1ee3d1995a689cfae8c819b6f84221c
wonj1012 <jjaa1012@gmail.com>
2023-08-11 02:18:46 +0900

    🎨style: Sort imports

--- app/models/annotations.py
+++ app/models/annotations.py
@@ -1,13 +1,11 @@
 from typing import Annotated
 
-from fastapi import Path, Depends
-
+from fastapi import Depends, Path
 from xrpl.asyncio.clients import AsyncJsonRpcClient
 
-from app.common.constants import XRPL_ADDRESS_REGEX
 from app.common.config import settings
+from app.common.constants import XRPL_ADDRESS_REGEX
 from app.xrpl.client import get_xrpl_client
-
 
 XrplAddress = Annotated[
     str,

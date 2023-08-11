5926a91cb9eae8b4a18ace053cf4065bd1a25553
wonj1012 <jjaa1012@gmail.com>
2023-08-11 12:45:49 +0900

    ✨feat: Change TokenAmount to Amount
    Amount can be either represent XRP and Token.

--- app/models/requests.py
+++ app/models/requests.py
@@ -1,6 +1,8 @@
+from typing import Annotated
+
 from pydantic import BaseModel, Field
 
-from app.models.types import TokenAmount
+from app.models.types import Amount
 
 
 class TokenSwapRequest(BaseModel):
@@ -8,7 +10,7 @@ class TokenSwapRequest(BaseModel):
     A model representing the request for a token swap.
     """
 
-    source: TokenAmount = Field(..., description="The source token amount details for the swap")
-    dest: TokenAmount = Field(..., description="The destination token amount details for the swap")
+    source: Annotated[Amount, Field(..., description="The source token amount details for the swap")]
+    dest: Annotated[Amount, Field(..., description="The destination token amount details for the swap")]
     secret: str
     public: str
--- app/models/types.py
+++ app/models/types.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field
+from typing import Optional
+
+from pydantic import BaseModel, validator
 
 
 class Wallet(BaseModel):
@@ -11,11 +13,21 @@ class Wallet(BaseModel):
     private_key: str
 
 
-class TokenAmount(BaseModel):
+class Amount(BaseModel):
     """
-    A model representing the amount details of a token.
+    A model representing the amount details of a token or XRP.
     """
 
-    currency: str = Field(..., description="The currency code or symbol of the token")
-    issuer: str = Field(..., description="The issuer's XRPL address")
-    value: str | int | float = Field(..., description="The amount value of the token")
+    symbol: str
+    issuer: Optional[str]
+    value: str | int | float
+
+    @validator("issuer", pre=True, always=True)
+    def set_issuer(cls, val, values):
+        # If the symbol is "XRP", the issuer is optional and can be None.
+        if values.get("symbol") == "XRP":
+            return val
+        # For other symbols, the issuer must be provided.
+        if not val:
+            raise ValueError("Issuer is required for non-XRP symbols.")
+        return val

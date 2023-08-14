67bd9bf7a8c52797ac2d6fb2d6e19c1d321417b3
wonj1012 <jjaa1012@gmail.com>
2023-08-14 20:06:48 +0900

    ✨feat: Add websocket client

--- app/xrpl/client.py
+++ app/xrpl/client.py
@@ -1,5 +1,5 @@
 from fastapi.responses import JSONResponse
-from xrpl.asyncio.clients import AsyncJsonRpcClient
+from xrpl.asyncio.clients import AsyncJsonRpcClient, AsyncWebsocketClient
 from xrpl.models.response import Response
 
 from app.common.config import settings
@@ -16,6 +16,19 @@ async def get_xrpl_client(url: str | None = None) -> AsyncJsonRpcClient:
         AsyncJsonRpcClient: An instance of AsyncJsonRpcClient connected to the specified XRPL environment.
     """
     return AsyncJsonRpcClient(settings.json_rpc_url if url is None else url)
+
+
+async def get_xrpl_ws_client(url: str | None = None) -> AsyncWebsocketClient:
+    """
+    Returns an AsyncWebsocketClient instance connected to the specified XRPL environment.
+
+    Args:
+        url (str, optional): The XRPL environment URL. Defaults to the URL obtained via get_url.
+
+    Returns:
+        AsyncWebsocketClient: An instance of AsyncWebsocketClient connected to the specified XRPL environment.
+    """
+    return AsyncWebsocketClient(settings.websocket_url if url is None else url)
 
 
 def create_json_response(response: Response) -> JSONResponse:

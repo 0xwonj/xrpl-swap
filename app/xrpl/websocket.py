e72190214d0cb7739e2bfe4d72b4531ecc171e62
wonj1012 <jjaa1012@gmail.com>
2023-08-14 20:23:16 +0900

    📝docs: Add docstrings and clear test codes

--- app/xrpl/websocket.py
+++ app/xrpl/websocket.py
@@ -1,19 +1,36 @@
 import asyncio
-from fastapi import Depends
-from xrpl.models import PathFind, PathFindSubcommand
+
 from xrpl.asyncio.clients import AsyncWebsocketClient
+from xrpl.models import PathFind, PathFindSubcommand
 
-from app.common.config import settings
 from app.models.types import Amount
-from app.xrpl.client import get_xrpl_ws_client
 
 
-async def listener(client, source_address, destination_address, send_amount, destination_amount):
+async def listener(
+    client: AsyncWebsocketClient,
+    source_address: str,
+    destination_address: str,
+    send_amount: Amount,
+    destination_amount: Amount,
+) -> None:
+    """
+    Listen to messages from an XRPL Websocket client and print the latest exchange rate
+    and transaction details when available.
+
+    Args:
+        client (AsyncWebsocketClient): The XRPL Websocket client to listen for messages.
+        source_address (str): The address of the sender in the transaction.
+        destination_address (str): The address of the recipient in the transaction.
+        send_amount (Amount): The amount of currency the sender is sending.
+        destination_amount (Amount): The amount of currency the recipient will receive.
+    """
     async for message in client:
-        if "alternatives" in message.result and message.result["alternatives"]:
-            alternative = message.result["alternatives"][0]
+        if "alternatives" in message and message["alternatives"]:
+            alternative = message["alternatives"][0]
             source_amount = float(alternative["source_amount"]["value"])
             print(f"Updated exchange rate: {source_amount}")
+            print(f"{source_address} -> {destination_address}")
+            print(f"{send_amount} -> {destination_amount}")
 
 
 async def exchange_rate_stream(
@@ -21,8 +38,19 @@ async def exchange_rate_stream(
     destination_address: str,
     send_amount: Amount,
     destination_amount: Amount,
-    client: AsyncWebsocketClient = Depends(get_xrpl_ws_client),
+    client: AsyncWebsocketClient,
 ) -> None:
+    """
+    Start a stream to listen for the exchange rate between two addresses for a specified amount.
+    This function creates a listener task to monitor messages and sends a `PathFind` request to the XRPL Websocket.
+
+    Args:
+        source_address (str): The address of the sender in the transaction.
+        destination_address (str): The address of the recipient in the transaction.
+        send_amount (Amount): The amount of currency the sender is sending.
+        destination_amount (Amount): The amount of currency the recipient will receive.
+        client (AsyncWebsocketClient, optional): The XRPL Websocket client to use.
+    """
     async with client as ws_client:
         asyncio.create_task(
             listener(ws_client, source_address, destination_address, send_amount, destination_amount)
@@ -41,14 +69,3 @@ async def exchange_rate_stream(
         await asyncio.sleep(5)  # or any other logic you want to perform while listening
 
     # now, outside of the context, the client is closed.
-
-
-if __name__ == "__main__":
-    asyncio.run(
-        exchange_rate_stream(
-            settings.wallet.address,
-            settings.wallet.address,
-            Amount(symbol="XRP", value="10"),
-            Amount(symbol="TST", issuer="rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd", value="25"),
-        )
-    )

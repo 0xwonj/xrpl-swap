0c1df9ee85273f65f07160b70874cc7b2e1259dc
wonj1012 <jjaa1012@gmail.com>
2023-08-16 02:40:53 +0900

    ✨feat: Add Redis dependency annotation

--- app/models/annotations.py
+++ app/models/annotations.py
@@ -1,10 +1,12 @@
 from typing import Annotated
 
+import redis.asyncio as redis
 from fastapi import Depends, Path
 from xrpl.asyncio.clients import AsyncJsonRpcClient
 
 from app.common.config import settings
 from app.common.constants import XRPL_ADDRESS_REGEX
+from app.database.redis import get_redis
 from app.xrpl.client import get_xrpl_client
 
 XrplAddress = Annotated[
@@ -18,3 +20,5 @@ XrplAddress = Annotated[
 ]
 
 XrplClient = Annotated[AsyncJsonRpcClient, Depends(get_xrpl_client)]
+
+Redis = Annotated[redis.Redis, Depends(get_redis)]

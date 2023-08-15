78460169bf1cd7349abf7c37b368e39286cb051f
wonj1012 <jjaa1012@gmail.com>
2023-08-16 02:26:53 +0900

    📝docs: Add redis configuration

--- app/common/config.py
+++ app/common/config.py
@@ -15,13 +15,20 @@ class CommonSettings(BaseSettings):
     Common settings for the API.
     """
 
+    # Project
     PROJECT_NAME: str = "XRPL Swap API"
     API_VERSION: str = "0.0.1"
     API_PREFIX: str = "/api/v1"
 
-    DEBUG_MODE: bool = False
+    # FastAPI
     LIVE_RELOAD: bool = False
 
+    # Redis
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
+
+    # XRP Ledger
     json_rpc_url: str
     websocket_url: str
     wallet: Wallet
@@ -41,9 +48,15 @@ class DevSettings(CommonSettings):
     Development settings for the API.
     """
 
-    DEBUG_MODE: bool = True
+    # FastAPI
     LIVE_RELOAD: bool = True
 
+    # Redis
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
+
+    # XRP Ledger
     json_rpc_url: str = "https://s.altnet.rippletest.net:51234"  # Testnet
     websocket_url: str = "wss://s.altnet.rippletest.net:51233"  # Testnet
     wallet: Wallet = Wallet(
@@ -58,9 +71,15 @@ class AmmDevSettings(CommonSettings):
     AMM-Devnet settings for the API.
     """
 
-    DEBUG_MODE: bool = True
+    # FastAPI
     LIVE_RELOAD: bool = True
 
+    # Redis
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
+
+    # XRP Ledger
     json_rpc_url: str = "https://amm.devnet.rippletest.net:51234"  # AMM-Devnet
     websocket_url: str = "wss://amm.devnet.rippletest.net:51233"
     wallet: Wallet = Wallet(
@@ -75,9 +94,15 @@ class ProdSettings(CommonSettings):
     Production settings for the API.
     """
 
-    DEBUG_MODE: bool = False
+    # FastAPI
     LIVE_RELOAD: bool = False
 
+    # Redis
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
+
+    # XRP Ledger
     json_rpc_url: str = "https://s1.ripple.com:51234"  # Mainnet
     websocket_url: str = "wss://s1.ripple.com"
     wallet: Wallet = Wallet(

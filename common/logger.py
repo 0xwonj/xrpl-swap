94e8f1be9f3dc512eb9dc1931b810c74e0a23c06
wonj1012 <jjaa1012@gmail.com>
2023-09-04 16:09:10 +0900

    🐛fix: Rename logging.py -> logger.py

--- common/logger.py
+++ common/logger.py
@@ -5,3 +5,8 @@ def get_logger(env: str) -> logger:
     logger.add("logs/app.log", rotation="500 MB")
     logger.info(f"Environment: {env}")
     return logger
+
+
+if __name__ == "__main__":
+    log = get_logger("dev")
+    log.info("Hello, world!")

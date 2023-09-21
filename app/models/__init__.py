5a65fb3bab02e831ea08277544bf6ca3c620f917
Wonjae Choi <choi@wonj.me>
2023-09-21 09:43:10 +0900

    ✨feat: Add offer create/cancel endpoints
    Co-authored-by: ysy970923 <ysy970923@gmail.com>

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from app.models.annotations import Redis, XrplAddress, XrplClient
-from app.models.requests import PaymentRequest
+from app.models.requests import (
+    OfferCancelRequest,
+    OfferCreateRequest,
+    OfferQualityRequest,
+    PaymentRequest,
+)
 from app.models.responses import AccountInfoResponse
--- app/models/requests.py
+++ app/models/requests.py
@@ -1,9 +1,10 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 from app.models.annotations import XrplAddress
-from xrpledger.models import Amount
+from xrpledger.data.tokens import tokens
+from xrpledger.models import Amount, Token
 
 
 class PaymentRequest(BaseModel):
@@ -23,3 +24,55 @@ class PaymentRequest(BaseModel):
 
     secret: str
     public: str
+
+
+class OfferQualityRequest(BaseModel):
+    """
+    A model representing the request for a token swap.
+    """
+
+    token_from: Token = Field(..., description="The source token for the swap", example=tokens["XRP"])
+
+    token_to: Token = Field(
+        ..., description="The destination token for the swap", example=tokens["USD.Gatehub"]
+    )
+
+    from_amount: float = Field(..., ge=0, description="The source token amount for the swap")
+
+    @validator("token_to", pre=True, always=True)
+    def validate_token_pair(cls, token_to: Token, values: dict) -> Token:
+        """
+        Validate token pair
+        """
+        token_from = values.get("token_from")
+        if token_from == token_to:
+            raise ValueError("token_from and token_to should be different")
+        return token_to
+
+
+class OfferCreateRequest(BaseModel):
+    """
+    A model representing the request for a token offer create.
+    """
+
+    account: XrplAddress = Field(..., description="The XRPL address of the account initiating the offer")
+
+    taker_gets: Amount = Field(..., description="The token amount that the taker gets")
+
+    taker_pays: Amount = Field(..., description="The token amount that the taker pays")
+
+    secret: str  # this is temporary
+    public: str  # this is temporary
+
+
+class OfferCancelRequest(BaseModel):
+    """
+    A model representing the request for a token offer create.
+    """
+
+    account: XrplAddress = Field(..., description="The XRPL address of the account initiating the offer")
+
+    offer_sequence: int
+
+    secret: str  # this is temporary
+    public: str  # this is temporary
